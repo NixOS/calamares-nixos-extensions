f4fdbfd9d5b6d6c34766dba3063816b8d87da5eb
s0me1newithhand7s <litvinovb0@gmail.com>
2025-05-27 19:18:25 +0300

    GNOME migration out of xserver

--- modules/nixos/main.py
+++ modules/nixos/main.py
@@ -119,8 +119,8 @@ cfggnome = """  # Enable the X11 windowing system.
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
 
 """
 
