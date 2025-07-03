6e283b24248cf4d9c6177499ca40badd2b0abd9b
rewine <luhongxu@deepin.org>
2025-07-03 19:21:09 +0800

    Revert "Add Deepin as an option on the Desktop section"
    This reverts commit fac0150c4bd2872fbd1f1e873b30f48fbdd6546d.

--- modules/nixos/main.py
+++ modules/nixos/main.py
@@ -209,15 +209,6 @@ cfgbudgie = """  # Enable the X11 windowing system.
 
 """
 
-cfgdeepin = """  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the Deepin Desktop Environment.
-  services.xserver.displayManager.lightdm.enable = true;
-  services.xserver.desktopManager.deepin.enable = true;
-
-"""
-
 cfgkeymap = """  # Configure keymap in X11
   services.xserver.xkb = {
     layout = "@@kblayout@@";
@@ -597,8 +588,6 @@ def run():
         cfg += cfglumina
     elif gs.value("packagechooser_packagechooser") == "budgie":
         cfg += cfgbudgie
-    elif gs.value("packagechooser_packagechooser") == "deepin":
-        cfg += cfgdeepin
 
     if (
         gs.value("keyboardLayout") is not None
